830d81c2306b2015e319a45cf993b234193fbbfe
thewrlck <thewrlck@proton.me>
2023-07-04 19:57:11 +0200

    update content id

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { decompressSync } from "fflate";
 
 const params = new URLSearchParams(window.location.search);
 
-fetch(params.get('q') || '/abc123').then(async (res) => {
+fetch(params.get('q') || '/content/b3615f06ff4964f4935b2b06734684dee914661e07975464651858ee8366eb26i0').then(async (res) => {
   const buf = await res.arrayBuffer();
   console.log(buf);
   document.write(new TextDecoder().decode(decompressSync(new Uint8Array(buf))));
